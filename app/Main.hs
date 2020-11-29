5e651d7a3a4d112c2fd10e1804d4433aadfaba5c
Jeingo <jeingo20@gmail.com>
2020-11-29 21:39:52 +0300

    I started to work with configurator

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
-import Lib
+import qualified Data.Configurator as C 
 
 main :: IO ()
-main = someFunc
+main = undefined
+
+startBot :: IO ()
+startBot = do
+  conf <- readConfig
+  fstIni  <- fstInizialize
+  let env = makeEnv
+  cycleBot
+  return ()
+
+
+readConfig = undefined
+fstInizialize = undefined
+makeEnv = undefined
+cycleBot = undefined
